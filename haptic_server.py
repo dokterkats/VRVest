a21aabb4f416a3c81e89aadc57138c01551ff27a
dokterkats <73393296+dokterkats@users.noreply.github.com>
2021-09-22 19:20:24 +0200

    First edit

--- haptic_server.py
+++ haptic_server.py
@@ -3,6 +3,19 @@ import websockets
 from websocket import create_connection
 import json
 from time import sleep
+import RPi.GPIO as GPIO
+
+Relay_Ch1 = 26
+Relay_Ch2 = 20
+Relay_Ch3 = 21
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(Relay_Ch1,GPIO.OUT)
+GPIO.setup(Relay_Ch2,GPIO.OUT)
+GPIO.setup(Relay_Ch3,GPIO.OUT)
+
 
 # For more information check out
 # https://github.com/bhaptics/tact-python
@@ -42,14 +55,13 @@ async def server(websocket, path):
                     for point in data['Submit'][0]['Frame']['DotPoints']:
                         motors += map_front_motors(point['Index'])
 
-                    # Turn the relevant motors on
-                    for motor in set(motors):
-                        ws.send("on_" + str(motor))
+                    # Turn the ems pad on
+                    GPIO.output(Relay_Ch1,GPIO.LOW)
                     # Sleep for the duration in milliseconds
                     sleep(duration/1000)
-                    # Then turn all the motors off
-                    for motor in set(motors):
-                        ws.send("off_" + str(motor))
+                    # Then turn the ems pad off
+                    GPIO.output(Relay_Ch1,GPIO.HIGH)
+
 
         except Exception as e:
             print(e)
